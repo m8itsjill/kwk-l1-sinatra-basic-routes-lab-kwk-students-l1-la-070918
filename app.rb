a9d5814e734cae9a7abefe2273cf7939015013b9
m8itsjill <jillyusi@icloud.com>
2018-07-17 18:29:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Jilliene Rae Yusi"
+end
+
+  get '/hometown' do
+    "My hometown is Los Angeles"
+end
+
+  get 'favorite-song' do
+    "My favorite song is Long Live, Ours, and Dear John"
+end
 end
